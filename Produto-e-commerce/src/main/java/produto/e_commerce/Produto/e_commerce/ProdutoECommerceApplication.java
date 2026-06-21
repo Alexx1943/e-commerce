5492f0efaaca17a69561171c4702fb3a8f835c87
alex <alextavares1943@gmail.com>
2026-06-21 18:50:00 -0300

    E-commerce

---
+++ Produto-e-commerce/src/main/java/produto/e_commerce/Produto/e_commerce/ProdutoECommerceApplication.java
@@ -0,0 +1,13 @@
+package produto.e_commerce.Produto.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoECommerceApplication.class, args);
+	}
+
+}
---
+++ Produto-e-commerce/src/test/java/produto/e_commerce/Produto/e_commerce/ProdutoECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package produto.e_commerce.Produto.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
